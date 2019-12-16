f01844bece23691fa9600bea7709621cfda670d7
agent47nh <nhone4@gmail.com>
2019-12-16 18:20:02 +0200

    Improved and made problem file parser more efficient.

--- Problem Creator and Parser/problem_file_parser.cpp
+++ Problem Creator and Parser/problem_file_parser.cpp
@@ -42,9 +42,8 @@ void GridStatus(int grid[][sizeOfGrid]){
     output_file.close();
 }
 
-
 ////////////////////////////////////////////////////////////////////////////////
-//      fileParser() function parses the problem.txt.                         //
+//      FileParser() function parses the problem.txt.                         //
 //      Integrate this function with your DFS algorithm to create             //
 //          'solution.txt' file.                                              //
 ////////////////////////////////////////////////////////////////////////////////
@@ -52,107 +51,102 @@ void GridStatus(int grid[][sizeOfGrid]){
 //============================================================================//
 //      DO NOT MAKE EDITS TO THE CODE BELOW THIS LINE UNLESS IMPROVING IT!    //
 
-void fileParser(){
-    ifstream import_data("problem.txt");
+
+void ParseCoordinates(){
     string line;
-    int b_pos=0, e_pos=0, key_coordinates=0, key_problems=0, problem_in_digits=0;
+    int e_pos=0, b_pos=0, key_coordinates;
+    ifstream import_data("problem.txt");
     if(import_data.is_open()){
-        while(!import_data.eof()) {
-            getline(import_data, line);
-            while((line.length() != e_pos) || key_coordinates == 1) {
-                int x_cord, y_cord;
-                b_pos = line.find("(", b_pos);
-                e_pos = line.find(")", e_pos);
-                if(e_pos == -1 || b_pos == -1) break;
-                if(line.at(b_pos+2) == ',' && line.at(e_pos-2) == ',') {
-                    x_cord = line.at(b_pos+1) -48; y_cord = line.at(e_pos-1) -48;
-                    cout << "(" << x_cord << ", " << y_cord << ")" << endl;
-                    grid[x_cord][y_cord] = 1;
-                }
-                else if(line.at(b_pos+3) == ',' && line.at(e_pos-3) == ','){
-                    string data="";
-                    data += line.at(b_pos+1);
-                    data += line.at(b_pos+2);
-                    istringstream(data) >> x_cord;
-                    data.clear();
-                    data += line.at(b_pos+4);
-                    data += line.at(b_pos+5);
-                    istringstream(data) >> y_cord;
-                    cout << "(" << x_cord << ", " << y_cord << ")" << endl;
-                    grid[x_cord][y_cord] = 1;
-                }
-                else if(line.at(b_pos+3) == ',' && line.at(e_pos-2) == ','){
-                    string data="";
-                    data += line.at(b_pos+1);
-                    data += line.at(b_pos+2);
-                    istringstream(data) >> x_cord;
-                    data.clear();
-                    data += line.at(b_pos+4);
-                    istringstream(data) >> y_cord;
-                    cout << "(" << x_cord << ", " << y_cord << ")" << endl;
-                    grid[x_cord][y_cord] = 1;
-                }
-                else if(line.at(b_pos+2) == ',' && line.at(e_pos-3) == ','){
-                    string data="";
-                    data += line.at(b_pos+1);
-                    istringstream(data) >> x_cord;
-                    data.clear();
-                    data += line.at(b_pos+3);
-                    data += line.at(b_pos+4);
-                    istringstream(data) >> y_cord;
-                    cout << "(" << x_cord << ", " << y_cord << ")" << endl;
-                    grid[x_cord][y_cord] = 1;
-                }
-                ++b_pos; ++e_pos;
-                if (e_pos >= line.length()) key_coordinates=1;
+        getline(import_data, line);
+        while((line.length() != e_pos) || key_coordinates == 1) {
+            int x_cord, y_cord;
+            b_pos = line.find("(", b_pos);
+            e_pos = line.find(")", e_pos);
+            if(e_pos == -1 || b_pos == -1) break;
+            if(line.at(b_pos+2) == ',' && line.at(e_pos-2) == ',') {
+                x_cord = line.at(b_pos+1) -48; y_cord = line.at(e_pos-1) -48;
+                grid[x_cord][y_cord] = 1;
             }
-            if(!key_problems){
-                getline(import_data, line);
-                problem_in_digits = stoi(line) - 1;
-                cout << "Number of problems in this file: " << problem_in_digits+1 << endl;
-                key_problems=1;
+            else if(line.at(b_pos+3) == ',' && line.at(e_pos-3) == ','){
+                string data="";
+                data += line.at(b_pos+1);
+                data += line.at(b_pos+2);
+                istringstream(data) >> x_cord;
+                data.clear();
+                data += line.at(b_pos+4);
+                data += line.at(b_pos+5);
+                istringstream(data) >> y_cord;
+                grid[x_cord][y_cord] = 1;
             }
-            while(problem_in_digits >= 0){
-                getline(import_data, line);
-                int x_coordinate0, y_coordinate0, 
-                    x_coordinate1, y_coordinate1, count=0, tracker=0;
-                string buf;
-                for(tracker=0; line.length() > count; count++){
-                    if(isdigit(line.at(count))){
-                        buf += line.at(count);
-                    } 
-                    if(!isdigit(line.at(count))){
-                        tracker++;
-                        if(tracker==1) x_coordinate0=stoi(buf);
-                        else if(tracker==2)y_coordinate0=stoi(buf);
-                        else if(tracker==3)x_coordinate1=stoi(buf);
-                        else if(tracker==4)y_coordinate1=stoi(buf);
-                        buf.clear();
-                    }
-                }
-                problem_in_digits--;
-                // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-                //  DO NOT MAKE EDITS TO THE CODE ABOVE THIS LINE UNLESS YOU ARE IMPROVING IT.
-                //  x_coordinate0 & y_coordinate0 are START COORDINATES.
-                //  x_coordinate1 & y_coordinate1 are END COORDINATES.
-                //  These coordinates are available each time loop iterates, make sure to store them in a
-                //      safe place before operating on it.
-                cout << "Start Coordinates\t: ("<< x_coordinate0 << ", " << y_coordinate0 << ")" << endl;       // <------- Make changes here as needed
-                cout << "End Coordinates\t\t: ("<< x_coordinate1 << ", " << y_coordinate1 << ")" << endl;       // <------- Make changes here as needed
-                //  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^  //
-                //                             MAKE CHANGES AS NEEDED HERE                             //
+            else if(line.at(b_pos+3) == ',' && line.at(e_pos-2) == ','){
+                string data="";
+                data += line.at(b_pos+1);
+                data += line.at(b_pos+2);
+                istringstream(data) >> x_cord;
+                data.clear();
+                data += line.at(b_pos+4);
+                istringstream(data) >> y_cord;
+                grid[x_cord][y_cord] = 1;
+            }
+            else if(line.at(b_pos+2) == ',' && line.at(e_pos-3) == ','){
+                string data="";
+                data += line.at(b_pos+1);
+                istringstream(data) >> x_cord;
+                data.clear();
+                data += line.at(b_pos+3);
+                data += line.at(b_pos+4);
+                istringstream(data) >> y_cord;
+                grid[x_cord][y_cord] = 1;
+            }
+            ++b_pos; ++e_pos;
+            if (e_pos >= line.length()) key_coordinates=1;
+        }
+    }
+    GridStatus(grid);                                           // Instrustions for this function ('GridStatus()') provided above. 
+    import_data.close();
+}
+
+int QuantityOfProblemsFinder(){
+    string line;
+    int problem_in_digits;
+    ifstream import_data("problem.txt");
+    for(int i=0; i<2;i++) getline(import_data, line);
+    istringstream(line)>>problem_in_digits;
+    import_data.close();
+    return problem_in_digits;
+}
+
+void ProblemCoordinates(){
+    ifstream import_data("problem.txt");
+    string buf, line;
+    int x_coordinate0, y_coordinate0, 
+        x_coordinate1, y_coordinate1, problem_in_digits = QuantityOfProblemsFinder();
+    for(int i=0; i<2;i++) getline(import_data, line);
+    while(problem_in_digits>0){
+        int count=0, tracker=0;
+        getline(import_data, line);
+        for(tracker=0; line.length() > count; count++){
+            if(isdigit(line.at(count))) buf += line.at(count);
+            if(!isdigit(line.at(count))){
+                tracker++;
+                if(tracker==1) istringstream(buf) >> x_coordinate0;
+                else if(tracker==2) istringstream(buf) >> y_coordinate0;
+                else if(tracker==3) istringstream(buf) >> x_coordinate1;
+                else if(tracker==4) istringstream(buf) >> y_coordinate1;
+                buf.clear();
             }
         }
-    } else perror("Error opening file!\n");
+    problem_in_digits--;
+    cout << "Coordinates: ("<< x_coordinate0 << ", " << y_coordinate0 << ")\t" << "===>\t("<< x_coordinate1 << ", " << y_coordinate1 << ")"  << endl;       // <------- Make changes here as needed
+    }
     import_data.close();
 }
 
 int main(){
-    for(int i=0;i<sizeOfGrid;i++)           //  <--- This loop creates the grid
-        for(int j=0;j<sizeOfGrid;j++){      //  <-----------------
-            grid[i][j] = x;                 //  <-----------------
-        }                                   //  <--- Loop Ends
-    fileParser();
-    GridStatus(grid);                       // Instrustions for this function ('GridStatus()') provided above. 
+    for(int i=0;i<sizeOfGrid;i++)                               //  <--- This loop creates the grid
+        for(int j=0;j<sizeOfGrid;j++) grid[i][j] = x;           //  <-----------------
+    ParseCoordinates();                                         //  <--- Loop Ends
+    cout << QuantityOfProblemsFinder() << endl;
+    ProblemCoordinates();
     return 0;
 }
