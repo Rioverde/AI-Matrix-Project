23cf3583d960a6a85eeeda27662c82a24d583782
Rioverde <abbasbabayev.m98@gmail.com>
2019-12-24 04:51:05 +0200

    Update for Arduino

--- Hardware/Arduino/main/main.ino
+++ Hardware/Arduino/main/main.ino
@@ -1,22 +1,14 @@
-//We always have to include the library
+ //We always have to include the library
 #include "LedControl.h"
+#include "SoftwareSerial.h"
 
 
-//Use pins 12,11,10 and 1 MAX7219
-// Pins: DIN,CLK,CS, # of Display connected
 LedControl lc=LedControl(12,11,10,4);
-
 boolean ON = true;
 boolean OFF = false;
 
-//Set brightness
-lc.setIntensity(0,8);
-//Waking up the module
-lc.shutdown(0,false);
-
-void setup(){
-  Serial.begin(9600);
-    /*
+void setup() {
+  /*
    The MAX72XX is in power-saving mode on startup,
    we have to do a wakeup call
    */
@@ -25,68 +17,82 @@ void setup(){
   lc.setIntensity(0,8);
   /* and clear the display */
   lc.clearDisplay(0);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+    Serial.begin(9600);
 }
 
+
 void loop(){
-
-//listen and connect to PC 
   if (Serial.available() > 0){
-	String info;
-	info = SerialStringUntill('#');
-	Serial.Print(info);
+  Serial.println("Connected");
+  String info;
+  char c;
+  while (c = Serial.read()) {
+      info += c;
+    }
+  Serial.println(info);
 
-	//parcing document in arduino
-	for(int i = 0; i < info.length(); i++) {
-	  int row = 0, col = 0;                                                       
+  //parcing document in arduino
+  for(int i = 0; i < info.length(); i++) {
+    int row = 0, col = 0;                                                                                                                                                        
 
-	  if(info[i] == '\n'){
-		  lc.clearDisplay(0);
-		  delay(3000);
-	  }
-		
-		if(info[i] == ' ') {
-			++i;
-			continue;
-		}
-		if(info[i] == '(') {
-			++i;
+    if(info[i] == '#'){
+      lc.clearDisplay(0);
+      break;
+    }
+    if(info[i] == '\n'){
+      lc.clearDisplay(0);
+      delay(3000);
+    }
+    
+    if(info[i] == ' ') {
+      ++i;
 
-			if(isDigit(info[i])) {
-				Serial.println(info[i]);
-				row = (int)info[i] - 48;
-			}
-			++i;
+      continue;
+    }
+    if(info[i] == '(') {
+      ++i;
 
-			if(info[i] == ',') {
-				++i;
-				if(isDigit(info[i])) {
-					Serial.println(info[i]);
-					col = (int)info[i] - 48; 
-				}
-				++i;
-					if(info[i] == ')') {
-					++i;
-					}
-				}
-			}
-					// Set LED on
-					Serial.print(row);
-					Serial.print(" ");
-					Serial.print(col);
+      if(isDigit(info[i])) {
+        Serial.println(info[i]);
+        row = (int)info[i] - 48;
+      }
+      ++i;
 
-					ls.setLed(0, row, col, ON);
-					delay(1000);
+      if(info[i] == ',') {
+        ++i;
+        if(isDigit(info[i])) {
+          Serial.println(info[i]);
+          col = (int)info[i] - 48;      
+        }
+        ++i;
+          if(info[i] == ')') {
+          ++i;
+          }
+        }
+      }
+     
 
-					// Option to do it pixel by pixel
-					ls.setLed(0, row, col, OFF);
-		}
+    //set LED on
+    Serial.print(row);
+    Serial.print(" ");
+    Serial.print(col);
 
-					delay(2000);
-					lc.clearDisplay(0);
-					delay(2000);
-	}
+    lc.setLed(0, row, col, ON);
+    delay(1000);
 
-	else	
-		Serial.Println("Connection Error: Check your port name in C++ code")
+    //If u want to pixel travel
+    lc.setLed(0, row, col, OFF);
+    
+   }
+  }
 
+  else
+    Serial.println("Error in Connection: Serial is not available");
+
+   delay(3000);
+   lc.clearDisplay(0);
+   delay(1000);
 }
