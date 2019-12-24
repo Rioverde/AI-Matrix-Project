038e4174c309ea10f6555dc41105ba313936ca2e
Rioverde <abbasbabayev.m98@gmail.com>
2019-12-24 23:48:41 +0200

    update

--- Hardware/Arduino/main/main.ino
+++ Hardware/Arduino/main/main.ino
@@ -1,8 +1,11 @@
  //We always have to include the library
 #include "LedControl.h"
 #include "SoftwareSerial.h"
+#define SERIAL_BUFFER_SIZE 256
 
 
+//Test
+//(0,0),(0,1),(0,2),(0,3),(0,4),(0,5),(0,6),(0,7)
 LedControl lc=LedControl(12,11,10,4);
 boolean ON = true;
 boolean OFF = false;
@@ -17,31 +20,29 @@ void setup() {
   lc.setIntensity(0,8);
   /* and clear the display */
   lc.clearDisplay(0);
-  Serial.begin(9600);
+  Serial.begin(115200);
+
 }
 
+String info;
+int row = 0, col = 0;
 
 void loop(){
 
-  if (Serial.available() > 0){
-  Serial.println("Connected");
-  String info;
-  char c;
-  while (c = Serial.read()) {
-      info += c;
-    }
-  Serial.println(info);
+  if (Serial.available()){
+    Serial.println("Connected");
+    info = Serial.readStringUntil('#');
+    Serial.println(info);
   } else
-    Serial.println("Error: Serial Connection is not available");
+      Serial.println("Error: Serial Connection is not available");
 
   //parcing document in arduino
-  for(int i = 0; i < info.length(); i++) {
-    int row = 0, col = 0;                                                                                                                                                        
+  for(int i = 0; i < info.length(); i++) {                                                                                                                                                        
 
     if(info[i] == '#'){
       lc.clearDisplay(0);
-      break;
     }
+    
     if(info[i] == '\n'){
       lc.clearDisplay(0);
       delay(3000);
@@ -49,23 +50,23 @@ void loop(){
     
     if(info[i] == ' ') {
       ++i;
-
       continue;
     }
+    
     if(info[i] == '(') {
       ++i;
 
       if(isDigit(info[i])) {
-        Serial.println(info[i]);
-        row = (int)info[i] - 48;
+        row = info[i] - '0';
+        Coordinates += row;
       }
       ++i;
 
       if(info[i] == ',') {
         ++i;
         if(isDigit(info[i])) {
-          Serial.println(info[i]);
-          col = (int)info[i] - 48;      
+          col = info[i] - '0';
+          Coordinates += col;
         }
         ++i;
           if(info[i] == ')') {
@@ -73,22 +74,16 @@ void loop(){
           }
         }
       }
-     
-
-    //set LED on
-    Serial.print(row);
-    Serial.print(" ");
-    Serial.print(col);
-
+      
+    //Light up Led with given Row and Column
     lc.setLed(0, row, col, ON);
-    delay(1000);
+    delay(2000);
 
-    //If u want to pixel travel
-    lc.setLed(0, row, col, OFF);
-    
-   }
+  }
+  
+  //Clean Display
+  delay(1000);
+  lc.clearDisplay(0);
+  delay(1000);
 
-   delay(3000);
-   lc.clearDisplay(0);
-   delay(1000);
 }
