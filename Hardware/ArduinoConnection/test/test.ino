7286e79a13b89b606f8fc8b5c90fb7b492ff1847
Rioverde <abbasbabayev.m98@gmail.com>
2019-12-24 04:21:20 +0200

    Added test.ino

--- Hardware/ArduinoConnection/test/test.ino
+++ Hardware/ArduinoConnection/test/test.ino
@@ -1,11 +1,11 @@
-//We always have to include the library
+ //We always have to include the library
 #include "LedControl.h"
+#include "SoftwareSerial.h"
 
 
 LedControl lc=LedControl(12,11,10,4);
 boolean ON = true;
 boolean OFF = false;
-
 
 void setup() {
   /*
@@ -13,25 +13,36 @@ void setup() {
    we have to do a wakeup call
    */
   lc.shutdown(0,false);
-  lc.shutdown(1,false);
   /* Set the brightness to a medium values */
   lc.setIntensity(0,8);
-  lc.setIntensity(1,8);
   /* and clear the display */
   lc.clearDisplay(0);
-  lc.clearDisplay(1);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+    Serial.begin(9600);
 }
 
+
 void loop(){
-String info = "(0,0),(0,1),(0,2),(1,2),(1,3),(2,3)";
-  Serial.begin(9600);
+  if (Serial.available() > 0){
+  Serial.println("Connected");
+  String info;
+  char c;
+  while (c = Serial.read()) {
+      info += c;
+    }
+  Serial.println(info);
 
   //parcing document in arduino
   for(int i = 0; i < info.length(); i++) {
     int row = 0, col = 0;                                                                                                                                                        
 
+    if(info[i] == '#'){
+      lc.clearDisplay(0);
+      break;
+    }
     if(info[i] == '\n'){
-
       lc.clearDisplay(0);
       delay(3000);
     }
@@ -62,21 +73,26 @@ String info = "(0,0),(0,1),(0,2),(1,2),(1,3),(2,3)";
           }
         }
       }
+     
 
     //set LED on
     Serial.print(row);
     Serial.print(" ");
     Serial.print(col);
-    one_init();
 
     lc.setLed(0, row, col, ON);
     delay(1000);
+
+    //If u want to pixel travel
+    lc.setLed(0, row, col, OFF);
     
    }
+  }
+
+  else
+    Serial.println("Error in Connection: Serial is not available");
 
    delay(3000);
    lc.clearDisplay(0);
-   lc.clearDisplay(1);
    delay(1000);
-  
 }
